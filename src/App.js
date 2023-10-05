58c6b01825674a1cab141767bd2572add94c80a9
ktk8916 <ktk8916@naver.com>
2023-10-05 12:16:36 +0900

    bbbb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { api } from "./apiClient";
 
 function App() {
+  const [form, setForm] = useState({ name: "", description: "" });
+  const [users, setUsers] = useState([]);
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const userSave = async (e) => {
+    e.preventDefault();
+    await api(`/api`, "POST", form);
+    getUser();
+  };
+
+  const getUser = async () => {
+    const data = await api(`/api`, "GET", {});
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        maxWidth: "500px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="name">name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={form.name}
+          onChange={onChangeHandler}
+        />
+        <label htmlFor="desc">description</label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          value={form.description}
+          onChange={onChangeHandler}
+        />
+        <button type="submit" onClick={userSave}>
+          제출
+        </button>
+      </form>
+      <div style={{ display: "flex" }}>
+        <h1>id</h1>
+        <h2>name</h2>
+        <h3>description</h3>
+      </div>
+      {users.map((user, i) => (
+        <div key={i} style={{ display: "flex" }}>
+          <h1>{user.id}</h1>
+          <h2>{user.name}</h2>
+          <h3>{user.description}</h3>
+        </div>
+      ))}
     </div>
   );
 }
