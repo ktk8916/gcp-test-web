8eb8370402e1c064e45bdc08635c83fddf667adf
ktk8916 <ktk8916@naver.com>
2023-10-06 15:06:03 +0900

    gggg

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8080"
-    : "http://34.170.217.245:8080";
+  process.env.NODE_ENV === "development" && "http://localhost:8080";
 
 const api = async (url, method, data) => {
   const response = await axios({
